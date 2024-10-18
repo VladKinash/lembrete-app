aeeaaf5690ac8c1a89ba38daaea79d52c04857f6
Vlad Kinash <vlad.kinash@icloud.com>
2024-10-18 22:09:54 +0300

    Started developing a database, revised the structs to fit the database

--- flashcard/flashcard.go
+++ flashcard/flashcard.go
@@ -1,15 +1,47 @@
 package flashcard
 
+import (
+	"database/sql"
+	//"errors"
+	"fmt"
+	//"log"
+	//"sync"
+
+	_ "github.com/mattn/go-sqlite3"
+)
+
+type Deck struct {
+	maxNewCards int32
+	maxReviews  int32
+}
+
+func newDeck(maxNewCards int32, maxReviews int32) Deck {
+	return Deck{
+		maxNewCards: maxNewCards,
+		maxReviews:  maxReviews}
+}
+
 type Flashcard struct {
 	Front       string
 	Back        string
 	EaseFactor  float32
-	Repetitions int // Changed to exported
+	Repetitions int
 	Interval    float32
 	NextReview  string
+	DeckID      string
+	CardId      string
 }
 
-func NewFlashcard(front string, back string, easeFactor float32, repetitions int, interval float32, nextReview string) Flashcard {
+func NewFlashcard(
+	front string,
+	back string,
+	easeFactor float32,
+	repetitions int,
+	interval float32,
+	nextReview string,
+	DeckID string,
+	CardId string,
+) Flashcard {
 	return Flashcard{
 		Front:       front,
 		Back:        back,
@@ -17,5 +49,30 @@ func NewFlashcard(front string, back string, easeFactor float32, repetitions int
 		Repetitions: repetitions,
 		Interval:    interval,
 		NextReview:  nextReview,
+		DeckID:      DeckID,
+		CardId:      CardId,
 	}
+}
+
+func insertCard(card Flashcard) error {
+
+	return nil
+}
+
+func openDB(dbName string) error {
+	dbName = "./" + dbName + ".db"
+	database, err := sql.Open("sqlite3", dbName)
+	if err != nil {
+		return fmt.Errorf("failed to open or create database %w", err)
+	}
+	defer database.Close()
+
+	if err = database.Ping(); err != nil {
+		return fmt.Errorf("failed to connect to the database")
+	}
+	return nil
+}
+
+func mainData() {
+
 }
