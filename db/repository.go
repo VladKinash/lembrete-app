c554f9a962b79b7d268f1809bd9d658575ebc6f5
Vlad Kinash <vlad.kinash@icloud.com>
2024-10-20 16:06:44 +0300

    major changes in database layer, added update, display, and fetch operations

--- db/repository.go
+++ db/repository.go
@@ -4,6 +4,7 @@ import (
 	m "Lembrete/models"
 	"database/sql"
 	"fmt"
+	"strconv"
 
 	//"log"
 	_ "modernc.org/sqlite"
@@ -90,5 +91,134 @@ func InsertDeck(db *sql.DB, deck m.Deck) error {
 	}
 
 	fmt.Println("Inserted deck", deck.Name)
+	return nil
+}
+
+func FetchAllCards(db *sql.DB, deckId int32) ([]m.Flashcard, error) {
+	var cards []m.Flashcard
+	rows, err := db.Query("SELECT * FROM Cards WHERE DeckID= ?", strconv.Itoa(int(deckId)))
+	if err != nil {
+		return nil, fmt.Errorf("the query that selects all cards has failed")
+	}
+	defer rows.Close()
+
+	for rows.Next() {
+		var card m.Flashcard
+		if err := rows.Scan(
+			&card.Id,
+			&card.Front,
+			&card.Back,
+			&card.EaseFactor,
+			&card.Repetitions,
+			&card.Interval,
+			&card.NextReview,
+			&card.DeckID); err != nil {
+			return nil, fmt.Errorf("failed to scan rows of the card")
+		}
+		cards = append(cards, card)
+	}
+	if err := rows.Err(); err != nil {
+		return nil, fmt.Errorf("there was an error when iterating over the rows: %v", err)
+	}
+	if len(cards) == 0 {
+		return nil, fmt.Errorf("the list of decks is empty")
+	}
+	return cards, nil
+}
+
+func FetchAllDecks(db *sql.DB) ([]m.Deck, error) {
+
+	var Decks []m.Deck
+	rows, err := db.Query("SELECT * FROM Decks")
+	if err != nil {
+		return nil, fmt.Errorf("the query that was to select all decks has failed: %v", err)
+	}
+	defer rows.Close()
+
+	for rows.Next() {
+		var deck m.Deck
+		if err := rows.Scan(
+			&deck.Id,
+			&deck.Name,
+			&deck.MaxNewCards,
+			&deck.MaxReviewsDaily,
+		); err != nil {
+			return nil, fmt.Errorf("there was an error when scanning the decks: %v", err)
+		}
+		Decks = append(Decks, deck)
+	}
+	if err := rows.Err(); err != nil {
+		return nil, fmt.Errorf("there was an error when iterating over rows")
+	}
+	if len(Decks) == 0 {
+		return nil, fmt.Errorf("the list of decks is empty")
+	}
+	return Decks, nil
+}
+
+func DisplayArrDecks(decks []m.Deck) error {
+	for _, deck := range decks {
+		fmt.Printf("Deck ID: %d, Name: %s, Max New Cards: %d, Max Reviews Daily: %d\n",
+			deck.Id, deck.Name, deck.MaxNewCards, deck.MaxReviewsDaily)
+	}
+	return nil
+}
+
+func DisplayArrCards(cards []m.Flashcard) error {
+	for _, card := range cards {
+		fmt.Printf("Card ID: %d, Front: %s, Back: %s, Ease Factor: %.2f, Repetitions: %d, Interval: %.2f, Next Review: %s, Deck ID: %s\n",
+			card.Id, card.Front, card.Back, card.EaseFactor, card.Repetitions, card.Interval, card.NextReview.Format("2006-01-02"), card.DeckID)
+	}
+	return nil
+}
+
+func UpdateCardRecords(db *sql.DB, cards []m.Flashcard) error {
+
+	stmt, err := db.Prepare(`
+	UPDATE Cards 
+	SET Front = ?, 
+		Back = ?, 
+		EaseFactor = ?, 
+		Repetitions = ?, 
+		Interval = ?, 
+		NextReview = ?, 
+		DeckID = ? 
+	WHERE ID = ?
+	`)
+	if err != nil {
+		return fmt.Errorf("there ws an error when preparing the statement to update card records: %v", err)
+	}
+	defer stmt.Close()
+
+	for _, card := range cards {
+		_, err = stmt.Exec(card.Id, card.Front, card.Back, card.EaseFactor, card.Repetitions, card.Interval, card.NextReview.Format("2006-01-02"), card.DeckID)
+		if err != nil {
+			return fmt.Errorf("there was an error when executing the statement: %v", err)
+		}
+	}
+
+	return nil
+}
+
+func UpdateDeckRecords(db *sql.DB, decks []m.Deck) error {
+	stmt, err := db.Prepare(`
+        UPDATE Decks 
+        SET name = ?, 
+            MaxNewCards = ?, 
+            MaxReviewsDaily = ? 
+        WHERE id = ?
+    `)
+	if err != nil {
+		return fmt.Errorf("there was an error when preparing the statement to update deck records: %v", err)
+	}
+	defer stmt.Close()
+
+	for _, deck := range decks {
+		_, err = stmt.Exec(deck.Name, deck.MaxNewCards, deck.MaxReviewsDaily, deck.Id)
+		if err != nil {
+			return fmt.Errorf("there was an error when executing the statement: %v", err)
+		}
+	}
+
 	return nil
 }
