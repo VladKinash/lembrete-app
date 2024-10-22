1bd90cf297fd253b31e3b316ff7e9a8b0d15b565
Vlad Kinash <vlad.kinash@icloud.com>
2024-10-22 18:35:11 +0300

    separated db initialization and table creation functions in a separate file

--- db/repository.go
+++ db/repository.go
@@ -1,7 +1,7 @@
 package repository
 
 import (
-	models "Lembrete/models" // Use a descriptive alias
+	models "Lembrete/models"
 	"database/sql"
 	"fmt"
 	"time"
@@ -9,52 +9,6 @@ import (
 	_ "modernc.org/sqlite"
 )
 
-func CreateTableDeck(db *sql.DB) error {
-	createTableQuery := `CREATE TABLE IF NOT EXISTS Decks (
-		id INTEGER PRIMARY KEY AUTOINCREMENT,
-		name TEXT,
-		MaxNewCards INTEGER,
-		MaxReviewsDaily INTEGER
-	);`
-
-	_, err := db.Exec(createTableQuery)
-	if err != nil {
-		return fmt.Errorf("failed to create Decks table: %v", err)
-	}
-	return nil
-}
-
-func CreateTableCard(db *sql.DB) error {
-	createTableQuery := `CREATE TABLE IF NOT EXISTS Cards (
-		id INTEGER PRIMARY KEY AUTOINCREMENT,
-		Front TEXT,
-		Back TEXT,
-		EaseFactor REAL,
-		Repetitions INTEGER,
-		Interval REAL,
-		NextReview TEXT,
-		DeckID INTEGER,
-		FOREIGN KEY (DeckID) REFERENCES Decks(id)
-	);`
-
-	_, err := db.Exec(createTableQuery)
-	if err != nil {
-		return fmt.Errorf("failed to create Cards table: %v", err)
-	}
-	return nil
-}
-
-func OpenDB(dbName string) (*sql.DB, error) {
-	dbName = "./" + dbName + ".db"
-	database, err := sql.Open("sqlite", dbName)
-	if err != nil {
-		return nil, fmt.Errorf("failed to open or create database: %w", err)
-	}
-	if err = database.Ping(); err != nil {
-		return nil, fmt.Errorf("failed to connect to the database: %v", err)
-	}
-	return database, nil
-}
 
 func InsertCard(db *sql.DB, card models.Flashcard) error {
 	if db == nil {
